de1d4f334c4b279b844e6746abe621af073dd864
Reks <fahadadsani@gmail.com>
2022-09-13 01:21:13 +0300

    commit 5

--- script.js
+++ script.js
@@ -13,14 +13,16 @@ async function main(){
 }
 
 main()
-
+function showUserPosts(id){
+    window.location.href = `${window.location.origin}/user.html`
+}
 function userHTML(user){
-    return `<div class="user-card">
+    return `<div class="user-card" onclick='showUserPosts(${user.id})'>
     <div class="user-card__container">
     <h3>${user.name}</h4>
         <p><b>Email:</b> ${user.email}</p>
         <p><b>Phone:</b> ${user.phone}</p>
-        <p><b>Website:</b> <a href="${user.website}" target="_blank">${user.website}</a></p>
+        <p><b>Website:</b> <a href="https://${user.website}" target="_blank">https://${user.website}</a></p>
     </div>
 </div>`
 }
